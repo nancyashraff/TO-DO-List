e443a7d35db645d82037a80991c566d817acd42a
Nancy Ashraf <nancy.sabry05@eng-st.cu.edu.eg>
2025-09-16 14:39:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
 const form = document.getElementById('task-form');
-const input = document.getElementById('task-input');
+const inputText = document.getElementById('task-text');
+const inputCategory = document.getElementById('task-category');
 const taskList = document.getElementById('task-list');
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 document.addEventListener('DOMContentLoaded', () => {
   tasks.forEach(task => {
-    addTaskToDOM(task.text, task.completed);
+    addTaskToDOM(task.text, task.category, task.completed);
   });
 });
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  addTask(input.value.trim());
-  input.value = '';
-  input.focus();
+  addTask(inputText.value.trim(), inputCategory.value.trim());
+  inputText.value = '';
+  inputCategory.value = '';
+  inputText.focus();
 });
 
-function addTask(text) {
-  if (text === '') return;
-  
+function addTask(text, category) {
+  if (text === '' || category === '') return;
+
   const textLower = text.toLowerCase();
   if (!tasks.some(task => task.text.toLowerCase() === textLower)) {
-    const newTask = { text, completed: false };
+    const newTask = { text, category, completed: false };
     tasks.push(newTask);
     saveTasks();
-    addTaskToDOM(text, false);
+    addTaskToDOM(text, category, false);
   }
 }
 
-function addTaskToDOM(text, completed) {
+function addTaskToDOM(text, category, completed) {
   const li = document.createElement('li');
-  
+  li.className = "list-group-item d-flex justify-content-between align-items-center";
+
   const span = document.createElement('span');
-  span.textContent = text;
-  if (completed) 
-    span.classList.add('completed');
-  
+  span.innerHTML = `<strong>${text}</strong> <em class="text-muted">[${category}]</em>`;
+  if (completed) span.classList.add('completed');
+
   span.addEventListener('click', () => toggleTaskCompletion(span, text));
-  
+
   const delBtn = document.createElement('button');
   delBtn.textContent = 'X';
-  delBtn.className = 'btn btn-danger';
+  delBtn.className = 'btn btn-danger btn-sm';
   delBtn.addEventListener('click', () => deleteTask(li, text));
-  
+
   li.appendChild(span);
   li.appendChild(delBtn);
   taskList.appendChild(li);
